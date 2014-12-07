5cd2955005c8ff4fa329cd56f67b43c2dcef856e
Josh Zacharias <josh.owie@gmail.com>
2014-12-07 10:29:53 -0800

    Fix Checkstyle errors.

--- src/main/java/org/terasology/oreGeneration/components/DepthPocketOreGenComponent.java
+++ src/main/java/org/terasology/oreGeneration/components/DepthPocketOreGenComponent.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2014 MovingBlocks
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.terasology.oreGeneration.components;
 
 import org.terasology.customOreGen.PDist;
@@ -27,13 +42,13 @@ public class DepthPocketOreGenComponent implements Component, CustomOreGenCreato
     public float angle = 1f;
     public float angleRange = 1f;
     public float multiplier = 1f;
-    public float multiplierRange = 0f;
+    public float multiplierRange;
     public float density = 0.7f;
     public float densityRange = 0.2f;
     public float noiseLevel = 0.2f;
     public float noiseLevelRange = 0.2f;
-    public float noiseCutoff = 0f;
-    public float noiseCutoffRange = 0f;
+    public float noiseCutoff;
+    public float noiseCutoffRange;
 
 
     @Override
@@ -63,10 +78,15 @@ public class DepthPocketOreGenComponent implements Component, CustomOreGenCreato
         // generate structures for the whole chunk, let the densityFacet filter out the depth stuff
         float amountOfDepthRangeCovered = (float) region.getRegion().sizeY() / (depthRange * 2);
         StructureDefinition structureDefinition = new PocketStructureDefinition(
-                new PDist(frequency * amountOfDepthRangeCovered, frequencyRange * amountOfDepthRangeCovered), new PDist(radius, radiusRange), new PDist(thickness, thicknessRange),
-                new PDist(region.getRegion().sizeY() / 2, region.getRegion().sizeY() / 2), new PDist(angle, angleRange),
-                new PDist(multiplier, multiplierRange), new PDist(density, densityRange),
-                new PDist(noiseLevel, noiseLevelRange), new PDist(noiseCutoff, noiseCutoffRange));
+                new PDist(frequency * amountOfDepthRangeCovered, frequencyRange * amountOfDepthRangeCovered),
+                new PDist(radius, radiusRange),
+                new PDist(thickness, thicknessRange),
+                new PDist(region.getRegion().sizeY() / 2, region.getRegion().sizeY() / 2),
+                new PDist(angle, angleRange),
+                new PDist(multiplier, multiplierRange),
+                new PDist(density, densityRange),
+                new PDist(noiseLevel, noiseLevelRange),
+                new PDist(noiseCutoff, noiseCutoffRange));
 
         return structureDefinition;
     }
